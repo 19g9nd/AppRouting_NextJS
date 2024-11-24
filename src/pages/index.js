a7ab5fa80cfbe53c3d48cfbae7a1620f22501bdd
19g9nd <ahadova2017@gmail.com>
2024-11-24 17:13:11 +0400

    Add categories and posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { categories } from '/data'
 export default function Home() {
-  return (
-    <>
-        <h1>Main Title</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>All Categories:</h1>
+            <ul>
+                {categories.map((category) => (
+                    <li key={category.id}>
+                        <Link href={`/category/${category.id}`}>
+                            {category.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
